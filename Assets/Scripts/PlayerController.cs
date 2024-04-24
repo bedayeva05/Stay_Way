f1dcb3598bd07abc5a3b321af0191b8c7f1c9ea6
few-m <maria.drobot@hotmail.com>
2024-04-24 23:03:59 +0300

    Player Controller
    Управление WASD, прыжок, движение камеры

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float gravity = 9.8f;
+    public float jumpforce;
+    public float speed;
+
+    private Vector3 _moveVector;
+    private float _fallVelocity = 0;
+    private CharacterController _characterController;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        MovementUpdate();
+        JumpUpdate();
+    }
+
+    void FixedUpdate()
+    {
+        MovementFixedUpdate();
+        JumpFixedUpdate();
+    }
+
+    private void MovementUpdate()
+    {
+        //Movement
+        _moveVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+    }
+
+    private void JumpUpdate()
+    {
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _fallVelocity = -jumpforce;
+        }
+    }
+
+    private void MovementFixedUpdate()
+    {
+        //Movement
+        _characterController.Move(_moveVector * speed * Time.deltaTime);
+
+    }
+
+    private void JumpFixedUpdate()
+    {
+        //Fall and jump
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+        if (_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
+    }
+}
