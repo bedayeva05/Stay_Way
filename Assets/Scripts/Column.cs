cac6d6c9049c472d1b5caf0c5ce233940d3f1e6b
few-m <maria.drobot@hotmail.com>
2024-05-05 17:38:22 +0300

    Columns in progress

---
+++ Assets/Scripts/Column.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Column : MonoBehaviour
+{
+    public float rotationSpeed = 10f;
+    private float _currentAngle;
+
+    public void RotateColumn()
+    {
+        _currentAngle = transform.eulerAngles.y;
+        while (transform.eulerAngles.y != _currentAngle + 90)
+        {
+            transform.Rotate(Vector3.back, rotationSpeed * Time.deltaTime);
+        }
+
+    }
+}
