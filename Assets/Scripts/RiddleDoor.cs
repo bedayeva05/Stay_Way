a53a0b0568e2851085fbcea58e16c9d156def26d
few-m <maria.drobot@hotmail.com>
2024-05-05 21:29:29 +0300

    Columns and riddle in the garden

---
+++ Assets/Scripts/RiddleDoor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiddleDoor : MonoBehaviour
+{
+    public List<Vector3> positions = new List<Vector3>();
+    public List<GameObject> gameObjects = new List<GameObject>();
+    private Dictionary<Vector3, GameObject> positionGameObjectMap = new Dictionary<Vector3, GameObject>();
+
+    void Start()
+    {
+        for (int i = 0; i < positions.Count; i++)
+        {
+            positionGameObjectMap.Add(positions[i], gameObjects[i]);
+        }
+    }
+
+    private bool CheckAllPositions()
+    {
+        bool allPositionsCorrect = true;
+
+        foreach (Vector3 position in positions)
+        {
+            if (positionGameObjectMap.ContainsKey(position))
+            {
+                GameObject gameObjectAtPosition = positionGameObjectMap[position];
+                if (gameObjectAtPosition.transform.position != position)
+                {
+                    allPositionsCorrect = false;
+                    break;
+                }
+            }
+        }
+
+        return allPositionsCorrect;
+    }
+
+    public void RiddleIsSolved()
+    {
+        if(CheckAllPositions())
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
