c5794b722ef8dc6c75ca1cf21f53bf15ae14be4e
few-m <maria.drobot@hotmail.com>
2024-05-08 18:54:49 +0300

    Button icons and cross-scene teleport

--- Assets/Scripts/InteractiveButton.cs
+++ Assets/Scripts/InteractiveButton.cs
@@ -13,18 +13,18 @@ using UnityEditor;
 public class InteractiveButton : MonoBehaviour
 {
     public UnityEvent action;
-    //public bool needsCondition;
-
     public GameObject buttonIcon;
 
     private const KeyCode ButtonToPress = KeyCode.F;
 
     private bool _playerIsNearby;
 
+    private Transform _playerTransform;
 
     private void Update()
     {
         UpdatePressButton();
+        UpdateButtonIconRotation();
     }
     private void UpdatePressButton()
     {
@@ -32,11 +32,20 @@ public class InteractiveButton : MonoBehaviour
         if (!Input.GetKeyDown(ButtonToPress)) return;
         action?.Invoke();
     }
-
+    private void UpdateButtonIconRotation()
+    {
+        if (_playerTransform != null)
+        {
+            Vector3 directionToPlayer = _playerTransform.position - buttonIcon.transform.position;
+            directionToPlayer.y = 0;
+            buttonIcon.transform.rotation = Quaternion.LookRotation(directionToPlayer) * Quaternion.Euler(0, 180, 0);
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (!other.gameObject.GetComponent<PlayerController>()) return;
         _playerIsNearby = true;
+        _playerTransform = other.transform;
         buttonIcon.SetActive(true);
     }
 
@@ -44,6 +53,7 @@ public class InteractiveButton : MonoBehaviour
     {
         if (!other.gameObject.GetComponent<PlayerController>()) return;
         _playerIsNearby = false;
+        _playerTransform = null;
         buttonIcon.SetActive(false);
     }
 
---
+++ Assets/Scripts/SceneChangerAndPlayerTeleporter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneChangerAndPlayerTeleporter : MonoBehaviour
+{
+    public string nextSceneName; 
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    public void ChangeSceneAndTeleportPlayer()
+    {
+       SceneManager.LoadScene(nextSceneName);
+
+        if (gameManager != null)
+        {
+            gameManager.TeleportPlayer(GameObject.FindGameObjectWithTag("Player"));
+        }
+    }
+}
