83268b2f59be1c94c37b42efda097836cf3b5953
few-m <maria.drobot@hotmail.com>
2024-05-19 20:32:53 +0300

    Choose_menu in progress

---
+++ Assets/Scripts/ChooseMenuButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseMenuButton : MonoBehaviour
+{
+    public int targetSceneIndex;
+    public void Telport(int targetTeleportPointIndex)
+    {
+        TeleportManager.Instance.TeleportPlayer(targetSceneIndex, targetTeleportPointIndex);
+    }
+}
---
+++ Assets/Scripts/SceneTeleportButton.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SceneTeleportButton : MonoBehaviour
+{
+    public UnityEvent action;
+    public GameObject buttonIcon;
+
+    public int targetSceneIndex;
+    public int targetTeleportPointIndex;
+
+
+    private const KeyCode ButtonToPress = KeyCode.F;
+
+    private bool _playerIsNearby;
+
+    private Transform _playerTransform;
+    private GameObject _player;
+
+    private GameObject _chooseMenuUI;
+
+    private void Update()
+    {
+        UpdatePressButton();
+        UpdateButtonIconRotation();
+    }
+
+    private void UpdatePressButton()
+    {
+        if (!_playerIsNearby) return;
+        if (!Input.GetKeyDown(ButtonToPress)) return;
+        _player.GetComponent<InteractiveMode>().PlayerControlFreeze();
+        EnableUI();
+        //TeleportManager.Instance.TeleportPlayer(targetSceneIndex, targetTeleportPointIndex);
+        //StartCoroutine(LoadAndTeleport(targetSceneIndex, targetTeleportPointIndex));
+    }
+
+    private void UpdateButtonIconRotation()
+    {
+        if (_playerTransform != null)
+        {
+            Vector3 directionToPlayer = _playerTransform.position - buttonIcon.transform.position;
+            directionToPlayer.y = 0;
+            buttonIcon.transform.rotation = Quaternion.LookRotation(directionToPlayer) * Quaternion.Euler(0, 180, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.GetComponent<PlayerController>()) return;
+        _player = other.gameObject;
+        _playerIsNearby = true;
+        _playerTransform = other.transform;
+        _chooseMenuUI = _player.transform.Find("UI").GetChild(0).gameObject;
+        buttonIcon.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.gameObject.GetComponent<PlayerController>()) return;
+        _playerIsNearby = false;
+        _playerTransform = null;
+        buttonIcon.SetActive(false);
+    }
+    private void EnableUI()
+    {
+        if (_chooseMenuUI != null)
+        {
+            _chooseMenuUI.SetActive(true);
+        }
+    }
+}
