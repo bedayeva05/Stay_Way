cc1b43e865682b0630ac319cd8b5912e1b72a286
few-m <maria.drobot@hotmail.com>
2024-05-25 17:01:55 +0300

    Scale can change between scenes

--- Assets/Scripts/TeleportManager.cs
+++ Assets/Scripts/TeleportManager.cs
@@ -38,7 +38,48 @@ public class TeleportManager : MonoBehaviour
         List<Transform> targetPointsObjects = targetPoints.GetTargetPoints();
         if (targetPointsObjects != null)
         {
+            if (sceneIndex == 1)
+            {
+                playerController.jumpforce = 3;
+                playerController.walkingSpeed = 2;
+                playerController.runningSpeed = 3;
+                float newYScale = 0.77f;
+                playerController.transform.localScale = new Vector3(playerController.transform.localScale.x, newYScale, playerController.transform.localScale.z);
+            }
+            if (sceneIndex == 2)
+            {
+                playerController.jumpforce = 3;
+                playerController.walkingSpeed = 4;
+                playerController.runningSpeed = 6;
+                float newYScale = 0.8f;
+                playerController.transform.localScale = new Vector3(playerController.transform.localScale.x, newYScale, playerController.transform.localScale.z);
+            }
+            if (sceneIndex == 3)
+            {
+                playerController.jumpforce = 5;
+                playerController.walkingSpeed = 5;
+                playerController.runningSpeed = 7;
+                float newYScale = 1f;
+                playerController.transform.localScale = new Vector3(playerController.transform.localScale.x, newYScale, playerController.transform.localScale.z);
+            }
+            if (sceneIndex == 4)
+            {
+                playerController.jumpforce = 3;
+                playerController.walkingSpeed = 4;
+                playerController.runningSpeed = 6;
+                float newYScale = 0.8f;
+                playerController.transform.localScale = new Vector3(playerController.transform.localScale.x, newYScale, playerController.transform.localScale.z);
+            }
+            if (sceneIndex == 5)
+            {
+                playerController.jumpforce = 3;
+                playerController.walkingSpeed = 2;
+                playerController.runningSpeed = 3;
+                float newYScale = 0.77f;
+                playerController.transform.localScale = new Vector3(playerController.transform.localScale.x, newYScale, playerController.transform.localScale.z);
+            }
             playerController.TriggerTeleport(targetPointsObjects[teleportPointIndex].transform);
+            
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
             playerUI.DisableChooseMenuUI();
