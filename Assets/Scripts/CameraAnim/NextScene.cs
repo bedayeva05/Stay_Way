151d3a1ecaf24d408810eec1445a5993652d981e
F-Style <103450297+just-f-style@users.noreply.github.com>
2024-05-25 23:44:55 +0300

    CutScenes

---
+++ Assets/Scripts/CameraAnim/NextScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+	public int scene;
+	public void SwitchScene()
+	{
+		SceneManager.LoadScene(scene);
+	}
+}
---
+++ Assets/Scripts/TimeScaler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScaler : MonoBehaviour
+{
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
+}
