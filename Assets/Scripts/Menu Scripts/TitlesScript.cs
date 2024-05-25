a899492dc1fb36bde85d39a71c5fc1b113f91ab2
few-m <maria.drobot@hotmail.com>
2024-05-25 22:51:21 +0300

    Menu and titles are done

---
+++ Assets/Scripts/Menu Scripts/TitlesScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitlesScript : MonoBehaviour
+{
+    public void LoadTitles()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
